347a1b4592110ea54a574a4f5128ca2e966d56c3
jmnote <opcore@gmail.com>
2020-10-11 19:10:45 +0900

    .

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,61 +12,61 @@ const banner = `/*!
 
 module.exports = function(grunt) {
 
-	grunt.loadNpmTasks('grunt-sass');
-	grunt.loadNpmTasks('grunt-concat-css');
-	grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-concat-css');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
 
-	grunt.initConfig({
-		sass: {
-			options: {
-				implementation: sass,
-				outputStyle: 'expanded',
-				precision: 6
-			},
-			flatly: {
-				src: ['build_flatly.scss'],
-				dest: 'temp/flatly.css'
-			},
-			darkly: {
-				src: ['build_darkly.scss'],
-				dest: 'temp/darkly.css'
-			}
-		},
-		concat_css: {
-			options: {
-			},
-			all: {
-				src: ["temp/*.css"],
-				dest: "dist/toggleswatch.css"
-			},
-		},
-		cssmin: {
-			options: {
-				level: {
-					1: {
-						keepSpecialComments: 0,
-						roundingPrecision: 6
-					}
-				}
-			},
-			dist: {
-				src: ['dist/toggleswatch.css'],
-				dest: 'dist/toggleswatch.min.css'
-			}
-		},
-	});
-	grunt.registerTask('reduce', '', function () {
-		const exec = require('child_process').execSync;
-		const result = exec('./reduce_darkly.sh', { encoding: 'utf8' });
-		grunt.log.writeln(result);
-	});
-	grunt.registerTask('default', 'build a theme from scss ', () => {
-		grunt.task.run([
-			'sass:flatly',
-			'sass:darkly',
-			'reduce',
-			'concat_css',
-			'cssmin:dist'
-		]);
-	});
+  grunt.initConfig({
+    sass: {
+      options: {
+        implementation: sass,
+        outputStyle: 'expanded',
+        precision: 6
+      },
+      flatly: {
+        src: ['build_flatly.scss'],
+        dest: 'temp/flatly.css'
+      },
+      darkly: {
+        src: ['build_darkly.scss'],
+        dest: 'temp/darkly.css'
+      }
+    },
+    concat_css: {
+      options: {
+      },
+      all: {
+        src: ["temp/*.css"],
+        dest: "dist/toggleswatch.css"
+      },
+    },
+    cssmin: {
+      options: {
+        level: {
+          1: {
+            keepSpecialComments: 0,
+            roundingPrecision: 6
+          }
+        }
+      },
+      dist: {
+        src: ['dist/toggleswatch.css'],
+        dest: 'dist/toggleswatch.min.css'
+      }
+    },
+  });
+  grunt.registerTask('reduce', '', function () {
+    const exec = require('child_process').execSync;
+    const result = exec('./reduce_darkly.sh', { encoding: 'utf8' });
+    grunt.log.writeln(result);
+  });
+  grunt.registerTask('swatch', 'build a theme from scss ', () => {
+    grunt.task.run([
+      'sass:flatly',
+      'sass:darkly',
+      'reduce',
+      'concat_css',
+      'cssmin:dist'
+    ]);
+  });
 };
